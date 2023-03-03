ade3c18acdccbb5eafb1441b7dbfc92e1456e476
Haomin Qin <hqin@midway3-login3.rcc.local>
2023-03-03 10:27:46 -0600

    new commit

--- run_rho_eq.sh
+++ run_rho_eq.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-Deltaarray=(0.05 0.01 0.1 0.5)
-Deltaarray=(300)
+Deltaarray=(0.005 0.05 0.01 0.1 0.5 1)
+# Deltaarray=(300)
 fractionarray=(0.0)
 
 actiontime=1
@@ -9,13 +9,14 @@ actiontime=1
 julia_name="newsets_twocapitals_rho.jl"
 python_name="plot_rho_3d.py"
 
-# rhoarray=(0.7 0.8 0.9 1.00001 1.1 1.2 1.3 1.4 1.5)
-rhoarray=(1.0)
+rhoarray=(0.7 0.8 0.9 1.00001 1.1 1.2 1.3 1.4 1.5)
+rhoarray=(0.9 1.00001 1.1)
+# rhoarray=(1.0)
 # rhoarray=(1.00001)
 
 gammaarray=(1.01 2.0 3.0 4.0 5.0 6.0 7.0 8.0)
 
-symmetric=1
+symmetric=0
 alpha_z_tilde_ex=0.0
 
 for Delta in ${Deltaarray[@]}; do
@@ -24,7 +25,7 @@ for Delta in ${Deltaarray[@]}; do
             for gamma in "${gammaarray[@]}"; do
                     count=0
 
-                    action_name="Standard_grid_sym_Delta_300_special_1"
+                    action_name="Standard_grid_asym_small_Delta_scale_1754_crit_6_low_eq_tol"
 
                     dataname="${action_name}_${Delta}_frac_${fraction}"
 
@@ -45,11 +46,11 @@ for Delta in ${Deltaarray[@]}; do
 #SBATCH --job-name=${Delta}_${gamma}
 #SBATCH --output=./job-outs/$job_name/${action_name}/Delta_${Delta}_frac_${fraction}/rho_${rho}_gamma_${gamma}.out
 #SBATCH --error=./job-outs/$job_name/${action_name}/Delta_${Delta}_frac_${fraction}/rho_${rho}_gamma_${gamma}.err
-#SBATCH --time=0-12:00:00
+#SBATCH --time=0-24:00:00
 #SBATCH --partition=caslake
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=8G
+#SBATCH --mem=9G
 
 module load julia/1.7.3
 module load python/anaconda-2020.11
